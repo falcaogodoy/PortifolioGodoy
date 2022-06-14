ce5f9b6834721fa63a0ac0dfa4788ed8055cc788
falcaogodoy <falcaogodoy@gmail.com>
2022-06-14 17:44:09 -0300

    aula140emeio

--- JAVA/ex140/src/entities/Client.java
+++ JAVA/ex140/src/entities/Client.java
@@ -1,4 +1,55 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Client {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+
+    private String nome;
+    private String email;
+    private Date aniversario;
+
+    public Client() {
+    }
+
+    public Client(String nome, String email, Date aniversario) {
+        this.nome = nome;
+        this.email = email;
+        this.aniversario = aniversario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getAniversario() {
+        return aniversario;
+    }
+
+    public void setAniversario(Date aniversario) {
+        this.aniversario = aniversario;
+    }
+
+    @Override
+    public String toString() {
+        return "Client : " +
+                "nome = " + nome + '\'' +
+                " email = " + email + '\'' +
+                " aniversario = " + sdf.format(aniversario) +
+                '.';
+    }
 }
