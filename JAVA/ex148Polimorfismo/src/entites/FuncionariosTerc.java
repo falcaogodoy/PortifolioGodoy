b56d168a6ad29035643ef9980c19edf7878ce791
falcaogodoy <falcaogodoy@gmail.com>
2022-06-22 14:44:13 -0300

    FinalEx01Polimorf

--- JAVA/ex148Polimorfismo/src/entites/FuncionariosTerc.java
+++ JAVA/ex148Polimorfismo/src/entites/FuncionariosTerc.java
@@ -8,4 +8,24 @@ public class FuncionariosTerc extends Funcionarios{
         super(nome, horas, valHoras);
         this.taxaAdicional = taxaAdicional;
     }
+
+    public FuncionariosTerc(String nome, Integer horas, Double valHoras) {
+        super(nome, horas, valHoras);
+    }
+
+    public Double getTaxaAdicional() {
+        return taxaAdicional;
+    }
+
+    public void setTaxaAdicional(Double taxaAdicional) {
+        this.taxaAdicional = taxaAdicional;
+    }
+
+    @Override
+    public Double pagamento(){
+
+        return super.pagamento() + taxaAdicional * 1.1;
+
+    }
+
 }
