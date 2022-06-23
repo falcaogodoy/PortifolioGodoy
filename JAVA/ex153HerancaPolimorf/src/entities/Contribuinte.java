89a8ffd7d282d8789f2a4ccf58b40360f2291c1b
falcaogodoy <falcaogodoy@gmail.com>
2022-06-23 16:03:10 -0300

    FinalProjetoIMposto

--- JAVA/ex153HerancaPolimorf/src/entities/Contribuinte.java
+++ JAVA/ex153HerancaPolimorf/src/entities/Contribuinte.java
@@ -1,5 +1,45 @@
 package entities;
 
-public class Contribuinte {
+public abstract class Contribuinte {
+	
+	private String nome;
+	private Double rendaAnual;
+	
+	
+	public Contribuinte(){
+		
+	}
+
+
+	public Contribuinte(String nome, Double rendaAnual) {
+		super();
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Double getRendaAnual() {
+		return rendaAnual;
+	}
+
+
+	public void setRendaAnual(Double rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+	
+	
+	public abstract double imposto();
+	
+	
 
 }
