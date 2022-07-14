7e704f83d34e7ba725cb00f1ad4b8d8d7dca45d1
falcaogodoy <falcaogodoy@gmail.com>
2022-07-14 11:16:02 -0300

    teste

---
+++ backend/src/test/java/com/godoy/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godoy.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
